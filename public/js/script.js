ad227ddc6e7eb2169172822352eb90a32071bdf8
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-12 12:03:33 -0700

    setting up being able to capture your voice with JS

---
+++ public/js/script.js
@@ -0,0 +1,18 @@
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.lang = "en-US";
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+document.querySelector("button").addEventListener("click", () => {
+  recognition.start();
+});
+
+recognition.addEventListener("result", (e) => {
+  let last = e.results.length - 1;
+  let text = e.results[last][0].transcript;
+
+  console.log("Confidence: " + e.results[0][0].confidence);
+});
