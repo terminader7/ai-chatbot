9f98f8cf317bd308d506e092c51065b4b96a9c20
Nader Ebrahim <nebrahim2424@gmail.com>
2024-06-12 12:28:11 -0700

    setting up api.ai

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ const server = app.listen(5000);
 app.length("/", (req, res) => {
   res.sendFile("index.html");
 });
+
+const apiai = require("apiai")(process.env.APIAI_TOKEN);
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (text) => {
+    let apiaiReq = apiai.textRequest(text, {
+      sessionId: process.env.APIAI_SESSION_ID,
+    });
+
+    apiaiReq.on("response", (response) => {
+      let aiText = response.result.fulfillment.speech;
+      socket.emit("bot reply", aiText); //Send the result back to the
+    });
+
+    apiaiReq.on("error", (error) => {
+      console.log(error);
+    });
+
+    apiaiReq.end();
+  });
+});
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,5 @@
+const socket = io();
+
 const SpeechRecognition =
   window.SpeechRecognition || window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -13,6 +15,8 @@ document.querySelector("button").addEventListener("click", () => {
 recognition.addEventListener("result", (e) => {
   let last = e.results.length - 1;
   let text = e.results[last][0].transcript;
+
+  socket.emit("chat message", text);
 
   console.log("Confidence: " + e.results[0][0].confidence);
 });
